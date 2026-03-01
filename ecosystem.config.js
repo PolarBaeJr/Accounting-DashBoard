fdec80398030e5d6f04a56d83520fff18b81af03
PolarBaeJr <matthewcheng2007@tahoo.com>
2026-02-28 21:37:00 -0800

    fix: set absolute cwd path in ecosystem config for Pi deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: 'abc-logistics',
       script: 'http-server',
-      args: '. -p 3001 -a 127.0.0.1 --cors -c-1',
+      args: '/home/polarbaejr/Demo/Accounting_Dashboard -p 3001 -a 127.0.0.1 --cors -c-1',
       interpreter: 'none',
+      cwd: '/home/polarbaejr/Demo/Accounting_Dashboard',
       watch: false,
       autorestart: true,
       env: {
